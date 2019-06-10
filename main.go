63e79fca16b2ef7a94000c3b6af4325a835e1839
Sebastian Rahlf <basti@redtoad.de>
2019-06-10 22:24:45 +0200

    Remove debug code.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"github.com/karalabe/hid"
 	"time"
@@ -22,49 +21,19 @@ func main() {
 
 	println(fmt.Sprintf("Connected to %s %s", device.Manufacturer, device.Product))
 
-	//grid := [][]int{
-	//	{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0},
-	//	{1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
-	//	{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
-	//	{1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
-	//	{1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
-	//	{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
-	//	{0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0},
-	//}
+	grid := [][]int{
+		{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0},
+		{1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+		{1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+		{1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+		{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+		{0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0},
+	}
 
 	for {
-		//	_ = DisplayGrid(grid, device)
-		//	time.Sleep(400 * time.Millisecond)
-
-		//////
-		for _, row := range []uint64{0x2001ffefffffd7f, 0x2021ffbbffff7df, 0x2041ffbbffffd7f, 0x2061ffeff000000} {
-			buffer := make([]byte, 8)
-			binary.BigEndian.PutUint64(buffer, row)
-			_, _ = device.Write(buffer)
-		}
-		println(1)
+		_ = DisplayGrid(grid, device)
 		time.Sleep(400 * time.Millisecond)
-
-		for _, row := range []uint64{0x2001ffeff1ffd7f, 0x2021ffbbf1ff7df, 0x2041ffbbf1ffd7f, 0x2061ffeff000000} {
-			buffer := make([]byte, 8)
-			binary.BigEndian.PutUint64(buffer, row)
-			_, _ = device.Write(buffer)
-		}
-		println(2)
-		time.Sleep(400 * time.Millisecond)
-
-		_ = DisplayGrid([][]int{
-			{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		}, device)
-		println(3)
-		time.Sleep(400 * time.Millisecond)
-
 	}
 
 }
